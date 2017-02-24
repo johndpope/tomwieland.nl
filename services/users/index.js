ff5e98e367ab31c3365cb593a1ef6accc88aee85
Tom Wieland <tom.wieland@gmail.com>
2017-02-24 18:03:41 +0100

    current state

--- services/users/index.js
+++ services/users/index.js
@@ -1,31 +1,32 @@
-const kafka = require('kafka-node')
-const ascoltatori = require('ascoltatori')
+const kafka = require('node-rdkafka')
 
-const KAFKA_HOST = process.env.KAFKA_HOST
-const KAFKA_PORT = process.env.KAFKA_PORT
+const KAFKA_BROKER_LIST = process.env.KAFKA_BROKER_LIST
 
-const kafkaOptions = {
-  type: 'kafka',
-  json: false,
-  kafka: kafka,
-  connectionString: `${KAFKA_HOST}:${KAFKA_PORT}`,
-  clientId: 'ascoltatori',
-  groupId: 'ascoltatori',
-  defaultEncoding: 'utf8',
-  encodings: {
-    image: 'buffer'
-  },
+const producer = new kafka.Producer({
+  'metadata.broker.list': KAFKA_BROKER_LIST,
+})
+
+const producerWriteStream = producer.getWriteStream('test')
+
+function formatError(error) {
+  return error.stack || error.message || error
 }
 
-console.log('Connecting with', kafkaOptions)
-ascoltatori.build(kafkaOptions, (error, pubsub) => {
-  console.log('Connected!')
-
-  pubsub.subscribe('test', () => {
-    console.log('ARGUMENTS', arguments)
-  })
-
-  pubsub.publish('test', 'VALUE', () => {
-    console.log('published')
-  })
+producerWriteStream.on('error', (error) => {
+  console.log(`Write Stream Error: ${formatError(error)}`)
 })
+
+function sendMessage() {
+  console.log('sendMessage')
+
+  const success = producerWriteStream.write(new Buffer('Awzum'))
+
+  console.log('sendMessage success')
+
+  if (!success) {
+    // TODO: What to do?
+    process.nextTick(sendMessage)
+  }
+}
+
+sendMessage()
